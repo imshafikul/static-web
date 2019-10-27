ff45c05cae27a3c20f0b03b8cfe0c0dcd2c74ccb
Shafikul Islam <shafikul.me@gmail.com>
2019-10-27 18:33:33 +0600

    Sass task in progress

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,41 @@ const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const source = require("vinyl-source-stream");
 const browserify = require("browserify");
+
+function sassTask() {
+  console.log("ddd");
+
+  return gulp
+    .src("./src/sass/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
+}
+
+function jsTask() {}
+
+function browserSyncTask(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000,
+    open: true
+  });
+
+  done();
+}
+
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
+
+function watchAllFiles() {
+  gulp.watch("./src/sass/**/*.scss", sassTask);
+  gulp.watch("./src/js/**/*.js", jsTask);
+  gulp.watch(["./**/*.html"], browserSyncReload);
+}
+
+gulp.task("watch", gulp.parallel(watchAllFiles, browserSyncTask));
